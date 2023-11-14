44a00f52f4f9f819b17864110b4044ee0b9d6a61
Aman Vishwakarma <aman.vishwakarma.dev@gmail.com>
2023-11-14 10:03:29 +0530

    error handler

--- src/middlewares/error-handler.js
+++ src/middlewares/error-handler.js
@@ -1,7 +1,10 @@
 export const errorHandler = (err, req, res, next) => {
-  res.status(500).json({
+  console.log("error handler...");
+  console.log(err);
+
+  res.status(501).json({
     error: {
-      msg: err.message,
+      msg: err.message || "something went wrong..",
       success: false,
     },
   });

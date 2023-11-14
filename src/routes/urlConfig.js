87100bf836d2d540ff4b05cc42fbdc9234c41d14
Aman Vishwakarma <aman.vishwakarma.dev@gmail.com>
2023-11-14 10:04:57 +0530

    app url configured for endpoints

--- src/routes/urlConfig.js
+++ src/routes/urlConfig.js
@@ -1,6 +1,12 @@
 const BASE_AUTH_ROUTE = "/auth";
+const BASE_USER_ROUTE = "/user";
 
 export const AUTH_ROUTES = {
   LOGIN: BASE_AUTH_ROUTE + "/login",
   REGISTER: BASE_AUTH_ROUTE + "/register",
+};
+
+export const USER_ROUTES = {
+  PROFILE: BASE_USER_ROUTE + "/profile/:id",
+  PROFILE: BASE_USER_ROUTE + "/profile/:id",
 };

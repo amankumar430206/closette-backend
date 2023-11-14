fa7d0c9bd643901e0e8c297184ed57487677083c
Aman Vishwakarma <aman.vishwakarma.dev@gmail.com>
2023-11-14 10:03:46 +0530

    app routes init

--- src/routes/appRoutes.js
+++ src/routes/appRoutes.js
@@ -1,12 +1,14 @@
 import { Router } from "express";
 import { InitRoute } from "./init.route.js";
 import { AuthRouter } from "./auth.route.js";
+import { UserRouter } from "./user.route.js";
 import { errorHandler } from "../middlewares/error-handler.js";
-
+import { BASE_URL } from "../const.js";
 const router = Router();
 
 router.use(InitRoute);
-router.use(AuthRouter);
+router.use(BASE_URL, AuthRouter);
+router.use(BASE_URL, UserRouter);
 
 // Error Handler Route
 router.use(errorHandler);

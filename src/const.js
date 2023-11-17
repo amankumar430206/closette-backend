159e5addf854ffcf0606e89b20b2828cd421af48
Aman Vishwakarma <aman.vishwakarma.dev@gmail.com>
2023-11-17 23:42:09 +0530

    defined app constants

--- src/const.js
+++ src/const.js
@@ -1,2 +1,14 @@
 export const API_VERSION = "v1";
 export const BASE_URL = "/api/" + API_VERSION;
+export const HEADERS = {
+  AUTH_TOKEN: "c-auth-token",
+};
+
+export const categories = [
+  { name: "casual" },
+  { name: "formal" },
+  { name: "ethnic" },
+  { name: "swimwear" },
+  { name: "dailywear" },
+  { name: "lingerie" },
+];

e70851775ba010a826d94d5f517336e220831f5f
Aman Vishwakarma <aman.vishwakarma.dev@gmail.com>
2023-11-27 15:46:49 +0530

    fileupload hanlder

---
+++ src/middlewares/fileUpload.js
@@ -0,0 +1,17 @@
+import multer from "multer";
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "src/uploads");
+  },
+  filename: (req, file, cb) => {
+    cb(
+      null,
+      new Date().toISOString().replace(/:/g, "-") +
+        "-" +
+        file.originalname.split(" ").join("_")
+    );
+  },
+});
+
+export const upload = multer({ storage });

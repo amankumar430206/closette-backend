43227033475dcf787ce8fcb5f4ea0f57a59b0d9c
Aman Vishwakarma <aman.vishwakarma.dev@gmail.com>
2023-12-28 00:11:47 +0530

    product schema update

--- src/models/Product.js
+++ src/models/Product.js
@@ -5,44 +5,40 @@ import { MODELS } from "./model-consts.js";
 const deepPopulate = mongooseDeepPopulate(mongoose);
 const Schema = mongoose.Schema;
 
-const ProductSchema = new mongoose.Schema({
-  title: {
-    type: String,
-    required: true,
+const ProductSchema = new mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: MODELS.USERS, // Reference to the Users model
+      required: true,
+    },
+    closette: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: MODELS.CLOSETTE, // Reference to the closette model
+      },
+    ],
+    category: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: MODELS.CATEGORY, // Reference to the category model
+    },
+    title: {
+      type: String,
+      required: true,
+    },
+    image: {
+      type: String, // URL or file path to the product image
+      required: true,
+    },
+    description: {
+      type: String,
+    },
+    color: {
+      type: String,
+    },
   },
-  section: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: MODELS.CLOSETTE_SECTION, // Reference to the section model
-    required: true,
-  },
-  closette: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: MODELS.CLOSETTE, // Reference to the closette model
-    required: true,
-  },
-  user: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: MODELS.USERS, // Reference to the Users model
-    required: true,
-  },
-  image: {
-    type: String, // URL or file path to the product image
-    required: true,
-  },
-  brand: {
-    type: String,
-    required: true,
-  },
-  price: {
-    type: Number,
-  },
-  description: {
-    type: String,
-  },
-  color: {
-    type: String,
-  },
-});
+  { timestamps: true }
+);
 
 ProductSchema.plugin(deepPopulate);
 

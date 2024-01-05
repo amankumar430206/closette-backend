cc9d9c59c683246f3703ffc5808a1bb4b2503cdc
Aman Vishwakarma <aman.vishwakarma.dev@gmail.com>
2024-01-06 01:26:48 +0530

    closette valiadtion update

--- src/models/validations/closette.validation.js
+++ src/models/validations/closette.validation.js
@@ -1,9 +1,7 @@
 import Joi from "joi";
 
 export const closetteValidatiion = Joi.object({
-  name: Joi.string().min(3).max(255).trim().required(),
-  location: Joi.string().trim(),
-  description: Joi.string().max(255).trim(),
   user: Joi.string(),
-  sections: Joi.array(),
+  name: Joi.string().min(3).max(255).trim().required(),
+  description: Joi.string().max(255).trim(),
 });

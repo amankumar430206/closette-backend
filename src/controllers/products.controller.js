294bffbd82e9d2559648c9fc0aab9c850d92b5c2
Aman Vishwakarma <aman.vishwakarma.dev@gmail.com>
2024-01-06 01:26:38 +0530

    - pagination - getByClosetteId - add product refactor -

--- src/controllers/products.controller.js
+++ src/controllers/products.controller.js
@@ -1,22 +1,50 @@
 import { Products } from "../models/Product.js";
 import { productValidationSchema } from "../models/validations/products.validation.js";
 import { Users } from "../models/Users.js";
-import {
-  s3,
-  PutObjectCommand,
-  bucketName,
-  generateFileName,
-  getSignedImageUrl,
-} from "../services/s3.js";
+import { getSignedImageUrl, uploadDocument } from "../services/s3.js";
+import { PaginateQuery } from "../utils/utils.js";
 
 export default {
   getAll: async (req, res, next) => {
     try {
-      let data = await Products.find({});
+      const pagination = await PaginateQuery(req.query, Products);
+
+      const data = await Products.find({})
+        .skip((pagination.page - 1) * pagination.pageSize)
+        .limit(pagination.pageSize);
 
       res.status(200).json({
         success: true,
         content: data,
+        ...pagination,
+      });
+    } catch (err) {
+      next(err);
+    }
+  },
+
+  filterProducts: async (req, res, next) => {
+    try {
+      const pagination = await PaginateQuery(req.query, Products);
+      const filterConditions = [
+        {
+          user: req.query?.user,
+        },
+        {
+          closette: req.query?.closette,
+        },
+      ];
+
+      const data = await Products.find({
+        $or: filterConditions,
+      })
+        .skip((pagination.page - 1) * pagination.pageSize)
+        .limit(pagination.pageSize);
+
+      res.status(200).json({
+        success: true,
+        content: data,
+        ...pagination,
       });
     } catch (err) {
       next(err);
@@ -26,7 +54,7 @@ export default {
   getById: async (req, res, next) => {
     try {
       let data = await Products.findOne({ _id: req.params.id });
-
+      data.image = getSignedImageUrl(data.image);
       res.status(200).json({
         success: true,
         content: data,
@@ -38,11 +66,34 @@ export default {
 
   getByUserId: async (req, res, next) => {
     try {
-      const data = await Products.find({ user: req.params.id });
-
+      const pagination = await PaginateQuery(req.query, Products);
+      const data = await Products.find({ user: req.params.id })
+        .skip((pagination.page - 1) * pagination.pageSize)
+        .limit(pagination.pageSize);
       res.status(200).json({
         success: true,
         content: data,
+        ...pagination,
+      });
+    } catch (err) {
+      next(err);
+    }
+  },
+
+  getByClosetteId: async (req, res, next) => {
+    try {
+      const pagination = await PaginateQuery(req.query, Products);
+
+      // since one closette belongs to one user, so directly we can find items blonging to that closette
+      const data = await Products.find({
+        closette: req.params.id,
+      })
+        .skip((pagination.page - 1) * pagination.pageSize)
+        .limit(pagination.pageSize);
+      res.status(200).json({
+        success: true,
+        content: data,
+        ...pagination,
       });
     } catch (err) {
       next(err);
@@ -53,8 +104,6 @@ export default {
     try {
       // can be retrieved from, token or body
       const userId = req.body.user || req.decoded._id;
-
-      console.log(req.file);
 
       const { error } = productValidationSchema.validate({
         ...req.body,
@@ -73,19 +122,12 @@ export default {
           msg: "bad request",
         });
 
-      //parse image and upload to s3
-      const _params = {
-        Bucket: bucketName,
-        Body: req.file.buffer,
-        Key: generateFileName(),
-        ContentType: req.file.mimetype,
-      };
-      // save image to s3
-      const command = new PutObjectCommand(_params);
+      //upload document to s3 bucket
+      const doc = await uploadDocument(req.file);
+      console.log("file uploaded", doc.uploaded);
 
-      const uploaded = await s3.send(command);
-
-      console.log("uploaded image props", uploaded);
+      // get signed url
+      const ImageUrl = await getSignedImageUrl(doc.filename);
 
       // assign product with the user
       const product = new Products({
@@ -95,7 +137,7 @@ export default {
         description: req.body.description,
         color: req.body.color,
         user: user._id,
-        image: _params.Key, // key is the image identifier
+        image: doc.filename, // key is the image identifier
       });
 
       const result = await product.save();
